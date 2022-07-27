03004eac343790a637add817927fc502e0f628ba
MashuShark <us1gfw.maryn@agmail.com>
2022-07-27 16:19:02 +0300

    delete clear cookies method

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,16 +4,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import pages.BasePage;
 import pages.FinancePage;
 import pages.HomePage;
 import pages.SearchResultPage;
 
 public class BaseTest {
     private WebDriver driver;
-
-    // clear all cookies and local storage after each iteration
-    public final boolean CLEAR_COOKIES_AND_STORAGE = true;
 
     public final boolean HOLD_BROWSER_OPEN = true;
     private static final String GOOGLE_URL = "https://www.google.com.ua";
@@ -33,15 +29,6 @@ public class BaseTest {
         driver.manage().window().maximize();
         driver.get(GOOGLE_URL);
     }
-
-//    @AfterTest
-//    public void clearCookiesAndLocalStorage() {
-//        if (CLEAR_COOKIES_AND_STORAGE) {
-//            JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
-//            driver.manage().deleteAllCookies();
-//            javascriptExecutor.executeScript("window.localStorage.clear();");
-//        }
-//    }
 
     @AfterMethod
     public void close() {
