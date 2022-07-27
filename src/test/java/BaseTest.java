ee8123a751244b409b64d91ba0cfea4914b5173c
MashuShark <us1gfw.maryn@agmail.com>
2022-07-27 21:47:11 +0300

    add RemoteWebDriver

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,12 +1,16 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.FinancePage;
 import pages.HomePage;
 import pages.SearchResultPage;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BaseTest {
     private WebDriver driver;
@@ -24,11 +28,25 @@ public class BaseTest {
     }
 
     @BeforeMethod
-    public void testsSetUp() {
-        driver = new ChromeDriver();
+    public void testsSetUp() throws MalformedURLException {
+//        driver = new ChromeDriver();
+
+
+        final ChromeOptions chromeOptions = new ChromeOptions();
+
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--disable-dev-shm-usage");
+        //chromeOptions.setCapability("browserless:token", "YOUR-API-TOKEN");
+
+        driver = new RemoteWebDriver(
+                new URL("http://chrome:3000/webdriver"),
+                chromeOptions
+        );
         driver.manage().window().maximize();
         driver.get(GOOGLE_URL);
     }
+
 
     @AfterMethod
     public void close() {
@@ -36,6 +54,7 @@ public class BaseTest {
             driver.quit();
         }
     }
+
 
     public HomePage getHomePage() {
         return new HomePage(getDriver());
